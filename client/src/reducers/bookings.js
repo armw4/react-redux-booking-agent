1be1fa49cde90c5ffb4d524d553d58874c57b538
Antwan Wimberly <antwan.wimberly@gmail.com>
2016-11-21 03:09:10 -0500

    actually handle adding the booking

--- client/src/reducers/bookings.js
+++ client/src/reducers/bookings.js
@@ -1,9 +1,11 @@
-import { RECEIVE_BOOKINGS } from '../actions/'
+import { RECEIVE_BOOKINGS, ADD_BOOKING } from '../actions/'
 
 export default (state = [], action) => {
   switch(action.type) {
     case RECEIVE_BOOKINGS:
       return action.bookings
+    case ADD_BOOKING:
+      return [...state, action.booking]
     default:
       return state
   }
