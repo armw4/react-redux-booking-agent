b448a08788767775ef55aeec731eabf6c034e9f5
Antwan Wimberly <antwan.wimberly@gmail.com>
2016-11-21 03:29:07 -0500

    handle DISMISS_SEARCH inside the search reducer as well (clear value from store)

--- client/src/reducers/search.js
+++ client/src/reducers/search.js
@@ -1,9 +1,13 @@
 import { SEARCH_BOOKINGS } from '../actions/'
 
-export default (state = null, action) => {
+const DEFAULT_STATE = null
+
+export default (state = DEFAULT_STATE, action) => {
   switch(action.type) {
     case SEARCH_BOOKINGS:
       return action.query
+    case DISMISS_SEARCH:
+      return DEFAULT_STATE
     default:
       return state
   }
